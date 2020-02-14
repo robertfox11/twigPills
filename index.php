934d2e84df0b01103ae2a6179b374ea5df6df791
robert Salazar <11rsahome@gmail.com>
2020-02-14 14:48:33 +0100

    add news iterando

--- index.php
+++ index.php
@@ -5,6 +5,23 @@ $loader = new \Twig\Loader\FilesystemLoader('./template');
 //     'index' => 'Hello {{ name }}!',
 // ]);r
 $twig = new \Twig\Environment($loader);
-$who = "hola cambrones";
+$news = Array ( 
+    Array ( 
+        "new" => "La NBA honra a Kobe en un All Star de nuevo formato", 
+        "datetime" => date('Y-m-d'),
+        "description" => "Los homenajes a la leyenda de los Lakers y los cambios en el sistema de puntuación marcarán el partido de las estrellas del domingo",
+        ), 
+    Array ( 
+        "new" => "Gual defiende el modelo de las fundaciones como propietarias", 
+        "datetime" => date('Y-m-d'),
+        "description" => "El presidente de CaixaBank destaca que la gestión se basa en una visión con 'mayores horizontes temporales'", 
+    ), 
+    Array ( 
+        "new" => "El COI no contempla el traslado de los Juegos de Tokio", 
+        "datetime" => date('Y-m-d'), 
+        "description" => "El organismo asegura que la mayoría de estrellas chinas están entrenando fuera de su país",
+    ) 
+);
+$who = "hola cabrones";
 
-echo $twig->render('form.html.twig', compact('who'));
+echo $twig->render('news.html.twig', compact('who', 'news'));
